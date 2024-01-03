077c6e5fe8a246917a1badd3da3650734d0f2ae0
Santhosh-Kumar-23 <santhoshmoorthycolanonline@gmail.com>
2024-01-03 11:58:03 +0530

    deleted dist folder

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-"use strict";
 //typescript basics types
-let id = 5;
-let company = "colan";
-let isCompanyOpen = true;
-let x = 2;
+var id = 5;
+var company = "colan";
+var isCompanyOpen = true;
+var x = 2;
 //array
-let ids = [1, 2, 3, 4, 5]; //number array
-let ellameVenum = ["1", 2, true]; //any array
+var ids = [1, 2, 3, 4, 5]; //number array
+var ellameVenum = ["1", 2, true]; //any array
 //tuple type
-let employee = [2, "santhosh", true];
+var employee = [2, "santhosh", true];
 //tuple array in repeat multiple in same type
-let employees = [
+var employees = [
     [1, "santhosh", true],
     [2, "love", true],
     [3, "preethi", true]
 ];
 //union (more than atleast one particular type)
-let eid;
+var eid;
 eid = 6;
 eid = "yyyyy";
 //enum(number constant) how to assign variable like constants -----> using enum
@@ -36,14 +35,14 @@ var dircetional2;
     dircetional2["left"] = "left";
     dircetional2["right"] = "right";
 })(dircetional2 || (dircetional2 = {}));
-let user = {
+var user = {
     id: 1,
     name: "santhohs"
 };
 //type assert (convert one type to another type)
-let x3 = 6;
-let comId = x3; //type 1
-let comId2 = x3; //type 2
+var x3 = 6;
+var comId = x3; //type 1
+var comId2 = x3; //type 2
 // using type in function and also returned in number type 
 function doMath(a, b) {
     return a + b;
